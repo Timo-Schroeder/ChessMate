944fea76a2fd38bad4e5fb7454d6432f41af01e4
Timo <mail@timo-schroeder.me>
2025-12-03 21:23:12 +0100

    fix: add imports to database for build_runner

--- lib/_shared/services/local/drift_database.dart
+++ lib/_shared/services/local/drift_database.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 import 'package:chessmate/_shared/services/local/tables/players.dart';
 import 'package:chessmate/_shared/services/local/tables/tournaments.dart';
+import 'package:chessmate/_features/tournaments/_models/tournament_format.dart';
+import 'package:chessmate/_features/players/_models/gender.dart';
+import 'package:chessmate/_features/players/_models/fide_title.dart';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
